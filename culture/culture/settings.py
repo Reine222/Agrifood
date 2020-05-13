b59ec6c0bd9257e97d750511a7119ebe44e48970
Reine <guelanin.koulai@uvci.edu.ci>
2020-05-13 10:32:14 +0100

    outil

--- culture/culture/settings.py
+++ culture/culture/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'culture.wsgi.application'
+# WSGI_APPLICATION = 'web: gunicorn culture.wsgi'
 
 
 # Database
@@ -171,3 +173,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, '../static_cdn')
 MEDIA_ROOT= os.path.join(BASE_DIR, '../media_cdn')
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
